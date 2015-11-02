77ff846b3ed5604317b711ced407c08ccaf391df
Ikuo Terado <eqobar@gmail.com>
2015-11-02 21:14:30 +0900

    Rewrite BitmapWorkerTask with RxAndroid and retrolambda

--- smartimage/src/main/java/com/eqot/android/utils/image/smartimage/BitmapWorkerTask.java
+++ smartimage/src/main/java/com/eqot/android/utils/image/smartimage/BitmapWorkerTask.java
@@ -3,11 +3,17 @@ package com.eqot.android.utils.image.smartimage;
 import android.content.ContentResolver;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ImageView;
 
 import java.lang.ref.WeakReference;
 
+import rx.Observable;
+import rx.Subscription;
+import rx.schedulers.Schedulers;
+import rx.android.schedulers.AndroidSchedulers;
+
+/*
 public class BitmapWorkerTask extends AsyncTask<Uri, Void, Bitmap> {
     private final WeakReference<ImageView> mImageViewReference;
     private final ContentResolver mContentResolver;
@@ -33,5 +39,55 @@ public class BitmapWorkerTask extends AsyncTask<Uri, Void, Bitmap> {
                 imageView.setImageBitmap(bitmap);
             }
         }
+    }
+}
+/**/
+
+public class BitmapWorkerTask {
+    private static final String TAG = "BitmapWorkerTask (Rx)";
+
+    private final WeakReference<ImageView> mImageViewReference;
+    private final ContentResolver mContentResolver;
+
+    public Uri uri = null;
+
+    private Subscription mSubscription;
+
+    public BitmapWorkerTask(ImageView imageView, ContentResolver cr) {
+        mImageViewReference = new WeakReference<>(imageView);
+        mContentResolver = cr;
+    }
+
+    public void execute(Uri imageUri) {
+        mSubscription = Observable.just(imageUri)
+                .subscribeOn(Schedulers.computation())
+                .map(this::getBitmap)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::setBitmap, this::onError);
+    }
+
+    public void cancel(boolean flag) {
+        mSubscription.unsubscribe();
+    }
+
+    public Bitmap getBitmap(Uri uri) {
+        Log.i(TAG, Thread.currentThread().getName());
+
+        this.uri = uri;
+
+        return SmartImageLoader.decodeUri(mContentResolver, uri, 128, 128);
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        if (mImageViewReference != null && bitmap != null) {
+            final ImageView imageView = mImageViewReference.get();
+            if (imageView != null) {
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    public void onError(Throwable e) {
+        Log.i(TAG, "" + e);
     }
 }
